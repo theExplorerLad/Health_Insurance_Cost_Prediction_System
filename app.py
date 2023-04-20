5e856c6b4833ca788a212a444464d0a1b1bef737
Deep <deepjyotidash947@gmail.com>
2023-04-20 22:40:47 +0530

    minor changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import pickle
-from flask import Flask, request, app, jsonify, render_template
+from flask import Flask, request, app, render_template
 import numpy as np
+import pickle
 
 app = Flask(__name__)                               # Initializing the flask app
 model = pickle.load(open('model.pkl', 'rb'))        # Loading the model
@@ -9,14 +9,21 @@ model = pickle.load(open('model.pkl', 'rb'))        # Loading the model
 def home():                                         # Defining the function to be called when home is accessed
     return render_template('home.html')             # Rendering the home.html file
 
-@app.route('/prediction_api', methods = ['POST'])   # Defining the predict_api page of our web-app
-def predict_api():                                  # Defining the function to be called when predict_api is accessed
-    data = request.json['data']                     # Getting the data from the POST request.
-    new_data = np.array(data.values()).reshape(1,-1)# Converting the data into a numpy array for processing.
-    output = model.predict(new_data)                # Getting the model's prediction on the data.
-    # print(output[0])
-    return jsonify(output[0])                      # Returning the output as a json file.
-    # return render_template('home.html', f'The predicted value is {output[0]}.')
+@app.route('/predict',methods=['POST'])
+def predict():
+    age = int(request.form["age"])
+    sex = int(request.form["sex"])
+    bmi = float(request.form["bmi"])
+    children = int(request.form["children"])
+    smoker = int(request.form["smoker"])
+    region = int(request.form["region"])
+
+    input_data = (age, sex, bmi, children, smoker, region)
+    input_array = np.asarray(input_data).reshape(1,-1)
+    prediction = model.predict(input_array)
+
+    return render_template('home.html',prediction_text = f'Your insurance cost is ${prediction[0]}')
+
 
 if __name__ == "__main__":                         # Defining the main function
     app.run(debug = True)                          # Running the app in debug mode
